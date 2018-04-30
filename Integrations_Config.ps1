83a47421fd87fb7103bbdbe49b86b5e2d1be9ddf
chadbaldwin <chad@chadbaldwin.net>
2018-04-29 20:08:19 -0700

    Add files via upload

---
+++ Integrations_Config.ps1
@@ -0,0 +1,10 @@
+{
+	"AccuweatherAPI" : {
+		"BaseURL" : "http://dataservice.accuweather.com",
+		"APIKey" : "APIKEYAPIKEYAPIKEIAPIKEI"
+	},
+	"LIFXAPI" : {
+		"BaseURL" : "https://api.lifx.com/v1/lights",
+		"APIKey" : "APIKEYAPIKEYAPIKEIAPIKEIAPIKEYAPIKEYAPIKEIAPIKEIAPIKEYAPIKEYAPIKEIAPIKEI"
+	}
+}
